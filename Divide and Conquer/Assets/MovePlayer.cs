1646fc47256ae0f6dff81495a3208bbc7ea2874d
TimmosQuadros <timm.rasmussen@gmail.com>
2017-03-16 14:07:36 +0100

    Added a player that follows the mousePosition.

---
+++ Divide and Conquer/Assets/MovePlayer.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovePlayer : MonoBehaviour {
+
+    public float speed = 3.0f;
+    private Vector3 targetPos;
+    // Use this for initialization
+    void Start () {
+        
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetMouseButton(0))
+        {
+            float distance = transform.position.z - Camera.main.transform.position.z;
+            targetPos = new Vector3(Input.mousePosition.x, Input.mousePosition.y, distance);
+            targetPos = Camera.main.ScreenToWorldPoint(targetPos);
+        }
+        gameObject.transform.position = Vector3.MoveTowards(transform.position, targetPos, speed * Time.deltaTime);
+    }
+}
+
